0fc39acffd63999fbba66acde872f95b9eaf1c56
Gojko Adzic <gojko@gojko.com>
2017-07-30 09:51:09 +0200

    strip extension spec

---
+++ spec/util/strip-extension-spec.js
@@ -0,0 +1,15 @@
+/*global describe, it, expect */
+'use strict';
+const stripExtension = require('../../src/util/strip-extension');
+describe('stripExtension', () => {
+	it('removes an extension from a path name', () => {
+		expect(stripExtension('something.md')).toEqual('something');
+		expect(stripExtension('dir/path/something.md')).toEqual('dir/path/something');
+		expect(stripExtension('something.md.jpg')).toEqual('something.md');
+		expect(stripExtension('something')).toEqual('something');
+		expect(stripExtension('dir/path/something')).toEqual('dir/path/something');
+		expect(stripExtension('')).toEqual('.');
+		expect(stripExtension('.')).toEqual('.');
+		expect(stripExtension('..')).toEqual('..');
+	});
+});
--- src/components/examples-repository.js
+++ src/components/examples-repository.js
@@ -1,8 +1,12 @@
 'use strict';
+const stripExtension = require('../util/strip-extension');
 module.exports = function ExamplesRepository(config, components) {
 	const fileRepository = components.fileRepository,
 		self = this;
 	self.getPageNames = function () {
-		return fileRepository.getDirContents(fileRepository.referencePath('examples'), fileRepository.isSourcePage);
+		return fileRepository.getDirContents(
+			fileRepository.referencePath('examples'),
+			fileRepository.isSourcePage
+		).map(stripExtension);
 	};
 };
